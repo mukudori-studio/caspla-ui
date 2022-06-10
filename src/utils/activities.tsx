7d2f5b58f053477ec39048a9caf867e308d84f6a
mikitama55 <mikisuke.tamashiro@crevo.jp>
2022-06-10 20:18:14 +0900

    活動区分の一覧をutilityに追加

---
+++ src/utils/activities.tsx
@@ -0,0 +1,21 @@
+const activities = [
+  { key: 'actor_actress', textJA: '俳優・女優' },
+  { key: 'comedian', textJA: '芸人' },
+  { key: 'model', textJA: 'モデル' },
+  { key: 'child_actor', textJA: '子役' },
+  { key: 'musician', textJA: 'ミュージシャン' },
+  { key: 'idol', textJA: 'アイドル' },
+  { key: 'bikini_model', textJA: 'グラビアアイドル' },
+  { key: 'voice_actor', textJA: '声優' },
+  { key: 'narrator', textJA: 'ナレーター' },
+  { key: 'reporter', textJA: '司会・MC・リポーター' },
+  { key: 'expert', textJA: '文化人・専門家' },
+  { key: 'dancer', textJA: 'ダンサー' },
+  { key: 'companion', textJA: 'コンパニオン' },
+  { key: 'parts_model', textJA: 'パーツモデル' },
+  { key: 'athlete', textJA: 'スポーツ選手' },
+  { key: 'influencer', textJA: 'インフルエンサー' },
+  { key: 'other', textJA: 'その他' },
+]
+
+export default activities
