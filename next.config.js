8a994da027937c1b0728cf77b6a1562472db8fcc
mikitama55 <mikisuke.tamashiro@crevo.jp>
2022-06-11 14:07:32 +0900

    vercel用に環境変数管理修正

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-loadEnv(process.env.APP_ENV)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
-/** @param {string} appEnv */
-function loadEnv(appEnv = process.env.APP_ENV) {
-  const env = {
-    ...require(`./env/env.${appEnv}`),
-    NEXT_PUBLIC_APP_ENV: appEnv,
-  }
-
-  Object.entries(env).forEach(([key, value]) => {
-    process.env[key] = value;
-  })
-}
