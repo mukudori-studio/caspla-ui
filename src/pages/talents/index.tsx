a1bdeb9834d13c0f760a68f9a2eb3fa4f1b26419
mikitama55 <mikisuke.tamashiro@crevo.jp>
2022-06-12 18:18:03 +0900

    swr追加

--- src/pages/talents/index.tsx
+++ src/pages/talents/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
+import useSWR, { Fetcher } from 'swr'
 import Link from "next/link"
 import Meta from '@/components/Meta'
 import Button from '@/components/atoms/Button'
 import styles from '@/styles/Talent.module.scss'
 
+const fetcher = (url: RequestInfo) => fetch(url).then(r => r.json())
+
 const Talents: NextPage = () => {
+
+  const { data, error } = useSWR('https://zipcloud.ibsnet.co.jp/api/search?zipcode=6640846', fetcher)
+
+  console.log(data, error)
+
   return (
     <div className={styles.container}>
       <Meta title="タレント一覧" />
