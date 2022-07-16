82af4fffb0b61db44d6021a371057dff41c99eb7
mikitama55 <mikisuke.tamashiro@crevo.jp>
2022-07-17 01:56:14 +0900

    fix storybook

--- src/components/molecules/SearchKeyword/index.stories.tsx
+++ src/components/molecules/SearchKeyword/index.stories.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import SearchKeyword from '.';
+import SearchKeyword from '.'
 
 export default {
   title: 'Molecules/SearchKeyword',
   component: SearchKeyword,
-} as ComponentMeta<typeof SearchKeyword>;
+} as ComponentMeta<typeof SearchKeyword>
 
-const Template: ComponentStory<typeof SearchKeyword> = () => <SearchKeyword />;
+const click = () => console.log('click')
+const Template: ComponentStory<typeof SearchKeyword> = () => <SearchKeyword onClick={click} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
