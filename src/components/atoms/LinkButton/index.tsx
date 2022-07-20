7f0b61387d9290b285fadbeb5b63d0d1fdb1cafd
mikitama55 <mikisuke.tamashiro@crevo.jp>
2022-07-20 23:16:10 +0900

    Buttonにパターン追加

--- src/components/atoms/LinkButton/index.tsx
+++ src/components/atoms/LinkButton/index.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/components/atoms/Button.module.scss'
 interface LinkButtonProps {
   type?: 'button' | 'submit'
   size?: 'small' | 'medium' | 'large'
-  color?: 'primary' | 'secondary' | 'default'
+  color?: 'primary' | 'secondary' | 'third' | 'forth' | 'default'
   weight?: 'normal' | 'bold'
   href: string
   text: string
