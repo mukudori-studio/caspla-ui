b1671332bc0560d3bb66e487b5b9ec826ba217e6
Darshana <darshanam@auradot.com>
2022-11-11 21:12:42 +0530

    company details updating UI

--- src/components/organisms/LoginedHeaderMenu/index.tsx
+++ src/components/organisms/LoginedHeaderMenu/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Router from 'next/router'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRecoilState, useResetRecoilState, useSetRecoilState } from 'recoil'
+import { useRecoilValue, useResetRecoilState } from 'recoil'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { toast } from 'react-toastify'
@@ -13,8 +13,8 @@ import styles from '@/styles/components/organisms/LoginedHeaderMenu.module.scss'
 const LoginedHeaderMenu = () => {
 
   const [showMenu, setToggleMenu] = useState(false)
-  const [session, setSession] = useRecoilState(userAtom)
-  const [sessionThumbnail, setThumbnailSession] = useRecoilState(thumbnailAtom)
+  const session = useRecoilValue(userAtom)
+  const sessionThumbnail = useRecoilValue(thumbnailAtom)
   const resetSession = useResetRecoilState(userAtom)
   const resetAccessToken = useResetRecoilState(accessTokenAtom)
   const resetSessionThumbnail = useResetRecoilState(thumbnailAtom)
@@ -44,10 +44,10 @@ const LoginedHeaderMenu = () => {
         <div className={styles['m-logined-header-menu__name']}><span>{session.fullName}</span></div>
         <div className={styles['m-logined-header-menu__belong']}><span>{session.companyName === '' ? '無所属' : session.companyName}</span></div>
         {
-          sessionThumbnail.thumbnailImage && sessionThumbnail.thumbnailImage !== '' ? (
+          sessionThumbnail && sessionThumbnail !== '' ? (
             <div className={styles['m-logined-header-menu__thumbnail']}>
               <Image
-                src={session.thumbnailImage}
+                src={sessionThumbnail}
                 layout="fill"
                 objectFit="contain"
               />
@@ -66,7 +66,7 @@ const LoginedHeaderMenu = () => {
             <li><Link href="/setting/edit/account"><a className={menuStyle} onClick={hideMenu}>アカウント設定</a></Link></li>
             { (session.role === 'TALENT' || session.role === 'PRODUCTION_TALENT') && <li><Link href="/setting/edit/profile"><a className={menuStyle} onClick={hideMenu}>プロフィール編集</a></Link></li> }
             { session.role === 'PRODUCTION_ADMIN' && <li><Link href="/setting/production/"><a className={menuStyle} onClick={hideMenu}>プロダクション管理</a></Link></li>}
-            { session.role === 'COMPANY_ADMIN' && <li><Link href="/setting/company/"><a className={menuStyle} onClick={hideMenu}>組織管理</a></Link></li> }
+            { session.role === 'COMPANY_ADMIN' && <li><Link href="/setting/company/edit"><a className={menuStyle} onClick={hideMenu}>組織管理</a></Link></li> }
             <li><button onClick={signOut} className={menuStyle}>ログアウト</button></li>
           </ul>
         </PopOver>
