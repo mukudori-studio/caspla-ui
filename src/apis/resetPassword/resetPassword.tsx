6bfb1d00d0adabe82bbba39fb03710ee9aef86f6
Darshana <darshanam@auradot.com>
2022-11-30 11:50:42 +0530

    changed API route

--- src/apis/resetPassword/resetPassword.tsx
+++ src/apis/resetPassword/resetPassword.tsx
@@ -4,12 +4,12 @@ const sendEmail = async (token: any, password: string, rePassword: string) => {
   try {
 
     const postData = {
-      token: token,
-      password: password,
-      rePassword: rePassword
+      'token': token,
+      'password': password,
+      'rePassword': rePassword
     }
 
-    const res = await axiosClient.post(`/api/v1/reset_password`, postData)
+    const res = await axiosClient.post(`/api/v1/reset_password/`, postData)
     return res
   } catch(err:any) {
     return err.response
