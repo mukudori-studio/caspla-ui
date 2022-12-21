929e90a971703e1e4e4e5ebc767393ebfa804915
Darshana <darshanam@auradot.com>
2022-12-21 08:59:30 +0530

    production details display and update

--- src/pages/setting/production/edit.tsx
+++ src/pages/setting/production/edit.tsx
@@ -11,6 +11,7 @@ import Loading from '@/components/atoms/Loading'
 import updateProductionDetails from '@/apis/productions/updateProductionDetails'
 import { toast } from 'react-toastify'
 import updateProductionLogo from '@/apis/images/updateProductionLogo'
+import { SAVED_CHANGES, SOMETHING_WENT_WRONG, CONTACT_SYS_ADMIN } from './../../../stores/messageAlerts/index';
 
 const ProductionEdit: NextPage = () => {
 
@@ -19,6 +20,17 @@ const ProductionEdit: NextPage = () => {
   const [links, setLinks] = useState<any>({})
   const [session, setSession] = useRecoilState(userAtom)
   const [productionId, setProductionId] = useState<number>(0)
+  
+  useEffect(()=>{
+    getProductionDetail(companyId)
+      .then((res)=> {
+        const {links, ...other} = res.response_message
+        setProductionId(other.id)
+        setLinks(links)
+        setProduction(other)
+      })
+      .catch((err)=> console.log(err))
+  },[])
 
   const updateProduction = (data:any) => {
     updateProductionDetails(data, companyId)
@@ -35,31 +47,17 @@ const ProductionEdit: NextPage = () => {
           companyName: other.productionName,
           isAdmin: session.isAdmin
         })
-        toast.success('詳細が正常に更新されました。', { autoClose: 3000, draggable: true})
+        toast.success(SAVED_CHANGES, { autoClose: 3000, draggable: true})
       })
       .catch((error)=> {
         console.log(error)
-        toast.error('エラーが発生しました。', { autoClose: 3000, draggable: true})
+        toast.error(SOMETHING_WENT_WRONG+CONTACT_SYS_ADMIN, { autoClose: 3000, draggable: true})
       })
     if(typeof data.companyImage === 'object') {
       updateProductionLogo(productionId, data.companyImage)
-        .then(()=> {
-          toast.success('画像が正常にアップロードされました。', { autoClose: 3000, draggable: true})
-        })
-        .catch(()=> toast.error('ファイルのアップロードでエラーが発生しました', { autoClose: 3000, draggable: true}))
+        .catch((error)=> console.log(error))
     }
   }
-
-  useEffect(()=>{
-    getProductionDetail(companyId)
-      .then((res)=> {
-        const {links, ...other} = res.response_message
-        setProductionId(other.id)
-        setLinks(links)
-        setProduction(other)
-      })
-      .catch((err)=> console.log(err))
-  },[])
 
   return (
     <main className={styles['p-production-setting']}>
