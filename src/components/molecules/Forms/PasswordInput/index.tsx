5e3742c89a5d890dd4652c14d7e32b1df1b89970
Darshana <darshanam@auradot.com>
2022-12-29 15:06:05 +0530

    changed message into japanese

--- src/components/molecules/Forms/PasswordInput/index.tsx
+++ src/components/molecules/Forms/PasswordInput/index.tsx
@@ -32,7 +32,7 @@ const PasswordInput = ({
           type={typeState}
           placeholder={placeholder}
           id={id}
-          {...register(id, { required: "必須項目です", minLength: { value: min, message: `${min}文字以上で入力してください。`}, validate : (value: string)=> !value.includes(" ") || 'Blanks are not Permitted.'})}
+          {...register(id, { required: "必須項目です", minLength: { value: min, message: `${min}文字以上で入力してください。`}, validate : (value: string)=> !value.includes(" ") || '不正な文字が含まれています。'})}
         />
         {/* TODO：パスワードの表示、非表示切り替えを行う */}
       </div>
