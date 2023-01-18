ca24b45c2653dd02901718afc45e1f84d3d3d856
Darshana <darshanam@auradot.com>
2023-01-18 21:48:45 +0530

    caspla ID validation imports

--- src/components/templates/CompanyFormTemplate.tsx
+++ src/components/templates/CompanyFormTemplate.tsx
@@ -14,11 +14,11 @@ import FormLabel from '@/components/atoms/Forms/Label'
 import FormNote from '@/components/atoms/Forms/Note'
 import ThumbnailUploader from '@/components/organisms/ThumbnailUploader'
 import prefectures from '@/utils/prefectures'
-
 import styles from '@/styles/AccountRegistration.module.scss'
 import { useRecoilValue } from 'recoil'
 import { userAtom } from '@/stores/Session'
-import { PRODUCTION_ID_AVAILABLE, PRODUCTION_ID_NOT_AVAILABLE, COMPANY_ID_AVAILABLE, COMPANY_ID_NOT_AVAILABLE, CASPLA_ID_LENGTH_REQUIRED } from '@/stores/messageAlerts/index'
+import { PRODUCTION_ID_AVAILABLE, PRODUCTION_ID_NOT_AVAILABLE, COMPANY_ID_AVAILABLE, COMPANY_ID_NOT_AVAILABLE, CASPLA_ID_LENGTH_REQUIRED, CASPLA_ID_VERIFICATION_ERROR } from '@/stores/messageAlerts/index'
+import { validateCasplaId } from './../../utils/validations';
 
 type InputProps = {
   companyImage?: any
@@ -137,10 +137,17 @@ const Signup = ({
   const changeLogo = (val: object) => setValue('companyImage', val)
 
   const onCheckId = () => {
-    if (userType === 'production') {
-      if(getValues('corpId').length<16 && getValues('corpId').length>=4) {
-        const strongCasplaId = new RegExp('(?=.*[a-zA-Z])(?=.*[0-9])')
-        if(strongCasplaId.test(getValues('corpId')) && getValues('corpId').search(/[\W]/g)===-1) {
+    switch (validateCasplaId(getValues('corpId'))) {
+      case 1:
+        setCheckId(false)
+        toast.error(CASPLA_ID_LENGTH_REQUIRED, { autoClose: 3000, draggable: true})  
+        break;
+      case 2: 
+        setCheckId(false)
+        toast.error(CASPLA_ID_VERIFICATION_ERROR, { autoClose: 3000, draggable: true})
+        break;
+      case 3:
+        if (userType === 'production') {
           checkProductionId(getValues('corpId'), companyId).then(() => {
             setCheckId(true)
             toast.success(PRODUCTION_ID_AVAILABLE, { autoClose: 3000, draggable: true})
@@ -149,17 +156,6 @@ const Signup = ({
             toast.error(PRODUCTION_ID_NOT_AVAILABLE, { autoClose: 3000, draggable: true})
           })
         } else {
-          setCheckId(false)
-          toast.error(PRODUCTION_ID_NOT_AVAILABLE, { autoClose: 3000, draggable: true})
-        }
-      } else {
-        setCheckId(false)
-        toast.error(CASPLA_ID_LENGTH_REQUIRED, { autoClose: 3000, draggable: true})
-      }
-    } else {
-      if(getValues('corpId').length<16 && getValues('corpId').length>=4) {
-        const strongCasplaId = new RegExp('(?=.*[a-zA-Z])(?=.*[0-9])')
-        if(strongCasplaId.test(getValues('corpId')) && getValues('corpId').search(/[\W]/g)===-1) {
           checkCorpId(getValues('corpId'), companyId).then((res) => {
             setCheckId(true)
             toast.success(COMPANY_ID_AVAILABLE, { autoClose: 3000, draggable: true})
@@ -167,14 +163,11 @@ const Signup = ({
             setCheckId(false)
             toast.error(COMPANY_ID_NOT_AVAILABLE, { autoClose: 3000, draggable: true})
           })
-        } else {
-          setCheckId(false)
-          toast.error(COMPANY_ID_NOT_AVAILABLE, { autoClose: 3000, draggable: true})
         }
-      } else {
+        break;
+      default:
         setCheckId(false)
-        toast.error(CASPLA_ID_LENGTH_REQUIRED, { autoClose: 3000, draggable: true})
-      }
+        break;
     }
   }
   
