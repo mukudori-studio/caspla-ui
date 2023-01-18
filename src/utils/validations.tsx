a7bd35c28c00b6f7ae52b6f6d24365bc511b817f
Darshana <darshanam@auradot.com>
2023-01-18 12:59:16 +0530

    caspla ID validation changes

--- src/utils/validations.tsx
+++ src/utils/validations.tsx
@@ -2,10 +2,11 @@ export const validateCasplaId = (value:string) => {
     if(value.length<16 && value.length>=4) {
         // const strongCasplaId = new RegExp('(?=.*[a-zA-Z])(?=.*[0-9])')
         // if(strongCasplaId.test(value) && value.search(/[\W]/g)===-1) {
-        if(value.includes('caspla')||value.includes('admin')||value.includes('root')) {
-            return 2;
-        } else {
+        let checkLetters = !value.includes('caspla')&&!value.includes('admin')&&!value.includes('root') 
+        if(checkLetters && value.search(/[\W]/g)===-1) {
             return 3;
+        } else {
+            return 2;
         }
     } else {
         return 1;
